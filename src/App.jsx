e78a21a4e8229ae9aac21a16e719478a76592c25
crystal boswell <cmboswell458@mail.cfcc.edu>
2026-03-22 19:12:25 -0400

    Initial React activity project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1>React Activity</h1>
+      <p>Crystal Michelle Boswell</p>
+      <p>March 22, 2026</p>
+    </main>
+  );
+}
+
+export default App;
